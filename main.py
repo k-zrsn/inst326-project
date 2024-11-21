2a2baab3a49cb402d63a6a8ead27546e85a62334
aborquezrodriguez <aborquez@terpmail.umd.edu>
2024-11-21 12:37:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -109,6 +109,12 @@ def set_week_availability(caregiver):
 ### Anthony
 import tkinter as tk
 from tkinter import ttk
+from random import choice
+
+#Calculating Weekly Pay
+#def weekly_pay()
+
+
 
 #Creating a Window 
 window =tk.Tk()
@@ -116,19 +122,27 @@ window.geometry('2000x1000')
 window.title("Weekly Pay and Monthly Totals")
 
 #Data
+caregivers = ['Anthony', 'Thomas', 'Virgil']
+Hourly_Pay = [20, 20, 20]
+Assigned_Hours = [40, 40, 40]
+Weekly_Pay = [800, 800, 800]
+Monthly_Pay = [3200, 3200, 3200]
 
 #Table 
-table = ttk.Treeview(window, columns = ('Name', 'Phone', 'Email', 'Pay Rate', 'Assigned Hours', 'Weekly Total', 'Monthly Total'), show = 'headings')
+table = ttk.Treeview(window, columns = ('Name', 'Pay', 'Assigned', 'Weekly', 'Monthly'), show = 'headings')
 table.heading('Name', text = 'Caregiver Name')
-table.heading('Phone', text = 'Caregiver Phone Number')
-table.heading('Email', text = 'Caregiver Email')
-table.heading('Pay Rate', text = 'Caregiver Hourly Pay')
-table.heading('Assigned Hours', text = 'Caregiver Weekly Hours')
-table.heading('Weekly Total', text = 'Caregiver Weekly Pay')
-table.heading('Monthly Total', text = 'Caregiver Monthly Pay')
+table.heading('Pay', text = 'Caregiver Hourly Pay')
+table.heading('Assigned', text = 'Caregiver Weekly Hours')
+table.heading('Weekly', text = 'Caregiver Weekly Pay')
+table.heading('Monthly', text = 'Caregiver Monthly Pay')
 table.pack(fill = 'both', expand = True)
 
-
+#Inserting Data
+for i in range(100):
+    Name = choice(caregivers)
+    table.insert(parent = '', index = 0, values = data)
+    
+    
 
 #Keeping Window Running
 window.mainloop()
